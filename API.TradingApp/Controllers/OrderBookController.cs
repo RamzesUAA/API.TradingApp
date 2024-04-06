9b1b211bf7efadf7602faa601068bceb4236ac4e
Roman Alberda <Roman.Alberda@harrods.com>
2024-04-06 20:37:48 +0300

    Added domain and persistence projects and draft order book controller

---
+++ API.TradingApp/Controllers/OrderBookController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.TradingApp.Controllers
+{
+    [Route("order-book")]
+    [ApiController]
+    public class OrderBookController : Controller
+    {
+        public OrderBookController()
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("= )");    
+        }
+    }
+}
